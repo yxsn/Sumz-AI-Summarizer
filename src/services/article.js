36f549d7906ebee30b520d7185cf84c9b3587fa5
Yasin N K <yasinnk15@gmail.com>
2024-05-21 03:22:18 +0530

    Demo.jsx updated

--- src/services/article.js
+++ src/services/article.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
@@ -11,13 +12,14 @@ export const articleApi = createApi({
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
       );
+
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) =>
-        `/summarize?url=${encodeURIComponents(params.articleUrl)}&length=3`,
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
